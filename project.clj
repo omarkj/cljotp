8d85ed9d151c1f8f3cf3ae552fec0c5bbfbbc1e4
Ómar Kjartan Yasin <omarkj@gmail.com>
2012-12-03 00:16:58 +0000

    Added a licence.

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject cljotp "0.1.0"
   :description "Create HOTP and TOTP"
-  :url "http://example.com/FIXME"
+  :url "http://github.com/omarkj/cljotp"
+  :licence "See the LICENCE file"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [commons-codec "1.6"]])
