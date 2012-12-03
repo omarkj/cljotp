260b4d8110a16d36426ad259e7ef2d1da88261d8
Ómar Kjartan Yasin <omarkj@gmail.com>
2012-12-03 00:17:27 +0000

    Implemented the SHA_MAC hash generator.

--- src/cljotp/crypto.clj
+++ src/cljotp/crypto.clj
@@ -1,5 +1,17 @@
 (ns cljotp.crypto
-  (:import (org.apache.commons.codec.binary Base32)))
+  (:import (org.apache.commons.codec.binary Base32)
+           (javax.crypto.spec SecretKeySpec)
+           (javax.crypto Mac)))
+
+(defn sha_mac
+  "Return the SHA1 MAC digest for a given key and data"
+  [secret, data]
+  (let [spec (SecretKeySpec. secret "HmacSHA1")
+        sha-mac (let [mac (Mac/getInstance "HmacSHA1")]
+                  (.init mac spec)
+                  mac)
+        hash (into [] (.doFinal sha-mac data))]
+    hash))
 
 (defn decode-base32
   "Decode a base32 string to binary."
