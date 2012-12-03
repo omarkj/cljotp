03354ec20f40d21ce7b18b62c363b1f28e2b2ff9
Ómar Kjartan Yasin <omarkj@gmail.com>
2012-12-03 00:17:50 +0000

    Implemented the pin returner.

--- src/cljotp/core.clj
+++ src/cljotp/core.clj
@@ -1,5 +1,6 @@
 (ns cljotp.core
-  (:import (java.util Date))
+  (:import (java.util Date)
+           (java.nio ByteBuffer))
   (:require (cljotp (crypto :as crypto))))
 
 (declare get-timestamp)
@@ -8,8 +9,16 @@
   "Get a one time token based on a secret and a
   interval number."
   [secret interval]
-  (println secret "Hello, World!"))
-
+  (let [secret (crypto/decode-base32 secret)
+        data (.array (.putLong (ByteBuffer/allocate 8) interval))
+        digest (crypto/sha_mac secret data)
+        offset (bit-and (digest 19) 0xf)
+        code (bit-or (bit-shift-left (bit-and (digest offset) 0x7f) 24)
+                     (bit-shift-left (bit-and (digest (+ offset 1)) 0xff) 16)
+                     (bit-shift-left (bit-and (digest (+ offset 2)) 0xff) 8)
+                     (bit-and (digest (+ offset 3)) 0xff))]
+      (rem code 1000000)))
+  
 (defn get-totp
   "Get a one time token based on the time."
   [secret]
@@ -19,4 +28,4 @@
 (defn get-timestamp
   "Convert milliseconds since epoch to a Unix Timestamp"
   [milliseconds]
-  ((int (/ milliseconds 1000))))
+  (int (/ milliseconds 1000)))
